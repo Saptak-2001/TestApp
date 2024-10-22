8ddc7b280f5818f7eb65c635ebc9f2c220daa829
SAPTAK SARKAR <115360675+Saptak-2001@users.noreply.github.com>
2024-10-22 21:27:32 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello from Amplify
+          Hello from Amplify (Modified)
         </a>
       </header>
     </div>
